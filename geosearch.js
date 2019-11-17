42ba339ab344209b3aa698195c3d890dd9b7d903
james tomisin <jamesoluwatomisinomotosho@gmail.com>
2019-11-18 02:10:15 +0400

    Get a static map image

--- geosearch.js
+++ geosearch.js
@@ -4,11 +4,26 @@
 /* eslint linebreak-style: ["error", "windows"] */
 /* eslint-disable-line prefer-template */
 /* eslint-disable  prefer-const */
+/* eslint-disable  no-use-before-define */
+/* eslint-disable   no-undef */
+let cityLog;
+let cityLat;
 function geocodeString() {
   let searchtext = document.getElementById('search-text').value;
   // eslint-disable-next-line prefer-template
   $.get('https://api.opencagedata.com/geocode/v1/json?q=' + searchtext + '&key=0dd5cc6ce0734c178fb21b728ad0711f', function (data, status) {
-    let cityLat = data.results[0].geometry.lat;
-    let cityLog = data.results[0].geometry.lng;
+    cityLat = data.results[0].geometry.lat;
+    cityLog = data.results[0].geometry.lng;
+    alert('request successful,please scroll down to view your result');
+    GetMap();
+  });
+}
+
+function GetMap() {
+  let map = new Microsoft.Maps.Map('#myMap', {
+    credentials: 'At4QSgi-CtS2RzIw1jTVBh7m-26b57Q_81HMxgDh1PSzcEXIzDLOylbG6iSGzdQd',
+    center: new Microsoft.Maps.Location(cityLat, cityLog),
+    mapTypeId: Microsoft.Maps.MapTypeId.aerial,
+    zoom: 10,
   });
 }
